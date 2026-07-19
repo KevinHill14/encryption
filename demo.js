d45543f687a0e7daedd31e8738babbbfe90fa7c7
Kevin <kevinhill016@outlook.com>
2026-07-18 20:15:54 -0400

    updated the encryption to work on files and command line input

--- demo.js
+++ demo.js
@@ -1,44 +1,65 @@
 const crypto = require("crypto");
 
-// Generate a salt
-// const salt = crypto.randomBytes(16) - Mthod for random salt
-const salt = Buffer.from("b09d2f3df658766d334281e03832f157", "hex");
+// Determine input
+const args = process.argv.slice(2)
+const mode = args[0]
+const password = args[1]
+const plain_text = args[2]
+
+const fs = require("fs");
 
 // Define variables
 const iterations = 250000
 const key_length = 32
 const digest = "sha256"
-const password = "helloWorld123"
-const plain_text = "Hello world, this is a secret!"
 const algorithm = "aes-256-gcm"
 
-// Generate a 256 bit key
-const derived_key = crypto.pbkdf2Sync(password, salt, iterations, key_length, digest)
+if (mode === "encrypt") {
+    // Generate Salt and iv
+    const salt = crypto.randomBytes(16)
+    const iv = crypto.randomBytes(12)
 
-// Generate a IV
-//const iv = crypto.randomBytes(12) - Method for random iv
-const iv = Buffer.from("ecb50bfefc3352c987eff433", "hex");
+    // Generate a 256 bit key
+    const derived_key = crypto.pbkdf2Sync(password, salt, iterations, key_length, digest)
 
-// Generate a cipher
-const cipher = crypto.createCipheriv(algorithm, derived_key, iv)
+    // Generate a cipher
+    const cipher = crypto.createCipheriv(algorithm, derived_key, iv)
 
-const encrypt_one = cipher.update(plain_text, 'utf8', 'hex')
-const encrypt_two = cipher.final('hex') 
-// const tag = cipher.getAuthTag() - Method for a random tag
-const tag = Buffer.from("c5214070403edcdb9356ef40ad7d4db2", "hex")
+    const encrypt_one = cipher.update(plain_text, "utf8", "hex")
+    const encrypt_two = cipher.final("hex")
+    const tag = cipher.getAuthTag()
 
-const cipher_text = encrypt_one + encrypt_two
-console.log(iv.toString('hex'))
-console.log(cipher_text)
-console.log(tag.toString('hex'))
+    const cipher_text = encrypt_one + encrypt_two
+    const cipher_text_buffer = Buffer.from(cipher_text, "hex")
+    const list = Buffer.concat([salt, iv, cipher_text_buffer, tag])
+    const data = list.toString("base64")
+    fs.writeFileSync("secret.vault", data)
+} 
+else if (mode === "decrypt") 
+{
+    const content = fs.readFileSync("secret.vault", "utf8")
+    const combined = Buffer.from(content, "base64")
+    const salt = combined.subarray(0, 16)
+    const iv = combined.subarray(16, 28)
+    const cipher_text = combined.subarray(28, combined.length - 16)
+    const tag = combined.subarray(combined.length - 16, combined.length)
 
-// Decrypt
-const decipher = crypto.createDecipheriv(algorithm, derived_key, iv)
-decipher.setAuthTag(tag)
-const decipher_text_one = decipher.update(cipher_text, "hex", "utf8")
-const decipher_text_two = decipher.final("utf8")
-const returned_msg = decipher_text_one + decipher_text_two
-console.log(returned_msg)
+    // Generate a 256 bit key
+    const derived_key = crypto.pbkdf2Sync(password, salt, iterations, key_length, digest)
 
-// NOTE - will need to go back to random IV + tag once I am done testing decryption, and I start working in files 
-// and can actually find the data there
+    const decipher = crypto.createDecipheriv(algorithm, derived_key, iv)
+    decipher.setAuthTag(tag)
+
+    const decipher_text_one = decipher.update(cipher_text, undefined, "utf8")
+    const decipher_text_two = decipher.final("utf8")
+    const returned_msg = decipher_text_one + decipher_text_two
+    console.log(returned_msg)
+}
+else
+{
+    console.log("Error in your format, try again!")
+}
+
+   // Decrypt
+    // const decipher = crypto.createDecipheriv(algorithm, derived_key, iv)
+    // decipher.setAuthTag(tag)
